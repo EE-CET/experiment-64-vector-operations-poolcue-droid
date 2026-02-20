b37a487ea0a6d1844dc94906b15e72c1cb757ac1
poolcue-droid <gaurigthampi@gmail.com>
2026-02-20 10:44:30 +0530

    added vector code

--- src/VectorOperations.java
+++ src/VectorOperations.java
@@ -15,27 +15,30 @@ public class VectorOperations {
             
             switch (choice) {
                 case 1:
-                    // TODO: Read the name and add it to the vector
-                    // Print "Added"
-                    
+                    // Add name
+                    String nameToAdd = scanner.next();
+                    students.add(nameToAdd);
+                    System.out.println("Added");
                     break;
                     
                 case 2:
-                    // TODO: Read the name and the 1-based index
-                    // Insert the name at the correct 0-based index in the vector
-                    // Print "Inserted"
-                    
+                    // Insert at 1-based index
+                    String nameToInsert = scanner.next();
+                    int index = scanner.nextInt();
+                    students.add(index - 1, nameToInsert); // convert to 0-based
+                    System.out.println("Inserted");
                     break;
                     
                 case 3:
-                    // TODO: Read the name and remove it from the vector
-                    // Print "Removed"
-                    
+                    // Remove name
+                    String nameToRemove = scanner.next();
+                    students.remove(nameToRemove);
+                    System.out.println("Removed");
                     break;
                     
                 case 4:
-                    // TODO: Display the vector
-                    
+                    // Display vector
+                    System.out.println(students);
                     break;
                     
                 default:
